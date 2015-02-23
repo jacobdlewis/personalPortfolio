967c73d7ff3fccc9f2d2d53da8793722c22c2c38
jacobdlewis <jacobdlewis@gmail.com>
2015-02-23 15:28:51 -0600

    grunt cleans public before compiling jade and copying to new public dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,24 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.initConfig({
+    clean: ['public'],
     copy: {
       main: {
         files: [
-          {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade'], dest: 'public/', filter: 'isFile'}
         ]
       }
     },
     jade: {
       compile: {
-      files: [{expand: true, cwd: 'app/', src: ['**/*.jade'], dest: 'public/', ext: '.html'}]
+      files: [{expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
       }
     }
   });
 
   grunt.registerTask('default', []);
+  grunt.registerTask('build', ['clean', 'copy', 'jade']);
 };
