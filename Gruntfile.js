ecfc886cb20da79f78250bcc2b8dd4ce67816ae3
jacobdlewis <jacobdlewis@gmail.com>
2015-02-23 15:16:31 -0600

    compiling jade with grunt works

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
 
   grunt.initConfig({
     copy: {
@@ -11,8 +12,12 @@ module.exports = function(grunt) {
           {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
         ]
       }
+    },
+    jade: {
+      compile: {
+      files: [{expand: true, cwd: 'app/', src: ['**/*.jade'], dest: 'public/', ext: '.html'}]
+      }
     }
-
   });
 
   grunt.registerTask('default', []);
